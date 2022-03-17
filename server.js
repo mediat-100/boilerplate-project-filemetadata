6543d6853cbba03bde363d7e570b1e3c1f870578
mediat-100 <yusuftomiwa740@gmail.com>
2022-03-17 10:57:33 +0100

    Handled file upload

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get("/", function (req, res) {
 	res.sendFile(process.cwd() + "/views/index.html");
 });
 
-app.post("/upload", function (req, res) {
+app.post("/api/fileanalyse", function (req, res) {
 	if (req.files.upfile) {
     let { name, mimetype, size } = req.files.upfile
 		res.status(200).json({

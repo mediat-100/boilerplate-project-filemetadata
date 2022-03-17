2c18ec3d5f56ef2cbe1d13b9c430a4683d053cd0
mediat-100 <yusuftomiwa740@gmail.com>
2022-03-17 10:42:31 +0100

    Handled file upload

--- server.js
+++ server.js
@@ -1,20 +1,34 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+var express = require("express");
+var cors = require("cors");
+var fileUpload = require("express-fileupload");
+require("dotenv").config();
 
 var app = express();
 
+app.use(
+	fileUpload({
+		limits: { fileSize: 50 * 1024 * 1024 },
+	})
+);
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+	res.sendFile(process.cwd() + "/views/index.html");
 });
 
-
-
+app.post("/upload", function (req, res) {
+	if (req.files.upfile) {
+    let { name, mimetype, size } = req.files.upfile
+		res.status(200).json({
+			"name": name,
+			"type": mimetype,
+			"size": size,
+		});
+	}
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+	console.log("Your app is listening on port " + port);
 });
